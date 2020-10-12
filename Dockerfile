8dc08c92c74e3efd7f403ae1b841e18232cbd707
shsankar4 <shsankar4@gmail.com>
2020-10-12 03:52:48 +0000

    Modified pom file

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:8-jdk-alpine
-COPY ./target/test_trading-0.0.1-SNAPSHOT.jar myapp.jar
+COPY ./target/trading-0.0.1-SNAPSHOT.jar myapp.jar
 ENTRYPOINT ["java","-jar","/myapp.jar"]
--- src/main/java/com/example/demo/controller/TradeController.java
+++ src/main/java/com/example/demo/controller/TradeController.java
@@ -4,14 +4,18 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.classes.Trade;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.FindIterable;
@@ -23,13 +27,13 @@ import com.mongodb.client.result.DeleteResult;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
-import com.example.demo.classes.Trade;
+
 @RestController
+@RequestMapping("/trade")
 @CrossOrigin
-@RequestMapping("/")
 public class TradeController {
-	MongoClientURI uri = new MongoClientURI("mongodb+srv://group4:admin@cluster0.1ikro.mongodb.net/tradedb?retryWrites=true&w=majority");
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public static MongoClientURI uri = new MongoClientURI("mongodb+srv://group4:admin@cluster0.1ikro.mongodb.net/tradedb?retryWrites=true&w=majority");
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
 	public String createTrade(@RequestBody Trade trade) {
 		Stock stock = null;
 		try {
@@ -42,22 +46,39 @@ public class TradeController {
 		}
 		else {
 			MongoClient myMongo = new MongoClient(uri);
-			MongoDatabase database = myMongo.getDatabase("trade_db");
+			MongoDatabase database = myMongo.getDatabase("tradedb");
 			
 			Document doc = new Document("date", trade.getDate()).append("ticker", trade.getStockTicker()).append("quantity",trade.getQuantity()).append("price", trade.getPrice()).append("status",trade.getStatus().toString());
-			MongoCollection<Document> mycollection = database.getCollection("trade");
+			MongoCollection<Document> mycollection = database.getCollection("trade_details");
 			mycollection.insertOne(doc);
 			myMongo.close(); 
 			return doc.getObjectId("_id").toString();
 		}
 	}
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public static String readTrade() {
+		System.out.println("Connecting.....");
+		MongoClient myMongo = new MongoClient(uri);
+		MongoDatabase database = myMongo.getDatabase("tradedb");
+		System.out.println("Connected");
+		
+		MongoCollection<Document> mycollection = database.getCollection("trade_details");
+		FindIterable<Document> iterDoc = mycollection.find();
+		Iterator<Document> it = iterDoc.iterator();
+		String s = "";
+		while(it.hasNext()) {
+			s+=it.next().toString()+ "\n";
+		}
+		myMongo.close();
+		return s;
+	}
 	@RequestMapping(value = "/update", method = RequestMethod.PUT)
-	public String updateTrade(@RequestBody Map<String,String> request) {
+	private static String updateDocument(@RequestBody Map<String,String> request) {
 		String id = request.get("id");
 		ObjectId objId = new ObjectId(id);
 		MongoClient myMongo = new MongoClient(uri);
-		MongoDatabase database = myMongo.getDatabase("trade_db");
-		MongoCollection<Document> mycollection = database.getCollection("trade");
+		MongoDatabase database = myMongo.getDatabase("tradedb");
+		MongoCollection<Document> mycollection = database.getCollection("trade_details");
 	
 		Document doc = new Document("status", Trade.status.EXECUTED.toString() );		
 		mycollection.updateOne(Filters.eq("_id", objId), new Document("$set", doc));
@@ -72,30 +93,14 @@ public class TradeController {
 		//PortfolioController.update(ticker, quantity, price);
 		return "Trade Executed";
 	}
-	@RequestMapping(value = "/view", method = RequestMethod.GET)
-	public String readTrade() {
-		System.out.println("Connecting.....");
-		MongoClient myMongo = new MongoClient(uri);
-		MongoDatabase database = myMongo.getDatabase("trade_db");
-		System.out.println("Connected");
-		
-		MongoCollection<Document> mycollection = database.getCollection("trade");
-		FindIterable<Document> iterDoc = mycollection.find();
-		Iterator<Document> it = iterDoc.iterator();
-		String s = "";
-		while(it.hasNext()) {
-			s+=it.next().toString()+ "\n";
-		}
-		myMongo.close();
-		return s;
-	}
+	
 	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-	public String deleteTrade(@RequestBody Map<String,String> request) {
+	private static String deleteTrade(@RequestBody Map<String,String> request) {
 		String id = request.get("id");
 		ObjectId objId = new ObjectId(id);
 		MongoClient myMongo = new MongoClient(uri);
-		MongoDatabase database = myMongo.getDatabase("trade_db");
-		MongoCollection<Document> mycollection = database.getCollection("trade");
+		MongoDatabase database = myMongo.getDatabase("tradedb");
+		MongoCollection<Document> mycollection = database.getCollection("trade_details");
 		DeleteResult deleteResult = mycollection.deleteOne(Filters.eq("_id",objId));
 		System.out.println(deleteResult.getDeletedCount());
 		myMongo.close();
